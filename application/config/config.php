7eef9622338483d2ebeeb818ae385eb2470c6805
Suastika Putra <wsuastika50@gmail.com>
2024-06-02 10:59:01 +0800

    perbaikan bug

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'localhost/adiabianvilla/';
-$config['base_url'] = 'http://localhost/PBL2/adiabianvilla/';
+$config['base_url'] = 'http://localhost/adiabianvilla/';
 
 // $root = "http://".$_SERVER['HTTP_HOST'];
 // $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
