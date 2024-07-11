811fd1cc6e62a85dcadf69bff2f50d80dd83ad84
BURGERKILL\krisnawayan <kwayan49@gmail.com>
2024-07-11 23:42:50 +0800

    about us

--- application/controllers/c_home.php
+++ application/controllers/c_home.php
@@ -8,4 +8,10 @@ class c_home extends CI_Controller {
        
         $this->load->view('home/index');
     }
+
+    public function about()
+    {
+        $this->load->view('home/about');
+        
+    }
 }
