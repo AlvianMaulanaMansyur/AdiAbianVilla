dd49eebbafdc40104cf54cc8f20ffd54dc508d1b
Suastika Putra <wsuastika50@gmail.com>
2024-07-18 22:52:08 +0800

    memperbarui link

--- application/views/kamar/detail_kamar.php
+++ application/views/kamar/detail_kamar.php
@@ -50,13 +50,13 @@
                                             <a href="<?php echo base_url('c_home/aboutus') ?>" class="sf-with-ul-pre">About Us</a>
                                         </li>
                                         <li class="menu-item hotale-normal-menu">
-                                            <a href="<?php echo base_url('c_home#villa_facilites') ?>">Facilities</a>
+                                            <a href="<?php echo base_url('home#villa_facilites') ?>">Facilities</a>
                                         </li>
                                         <li class="menu-item current-menu-item menu-item-has-children hotale-normal-menu">
                                             <a href="<?php echo base_url('room') ?>" class="sf-with-ul-pre">Reservation</a>
 
                                         </li>
-                                        <li class="menu-ite hotale-normal-menu"><a href="<?php echo base_url('c_home#villa_contact') ?>" class="sf-with-ul-pre">Contact</a></li>
+                                        <li class="menu-ite hotale-normal-menu"><a href="<?php echo base_url('home#villa_contact') ?>" class="sf-with-ul-pre">Contact</a></li>
                                     </ul>
                                     <div class="hotale-navigation-slide-bar hotale-navigation-slide-bar-style-2 hotale-left" data-size-offset="0" data-width="19px" id="hotale-navigation-slide-bar"></div>
                                 </div>
